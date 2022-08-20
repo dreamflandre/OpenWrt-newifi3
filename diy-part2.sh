d8b3cc1e597b842b1b42c5782848688393577702
dreamflandre <57754424+dreamflandre@users.noreply.github.com>
2022-08-20 16:51:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,8 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#修改主题，去除用户防火墙规则
+sed -i '/luci.main.lang/a uci set luci.main.mediaurlbase=/luci-static/argon' package/lean/default-settings/files/zzz-default-settings
+sed -i '/luci.main.lang/a uci set luci.themes.Argon=/luci-static/argon' package/lean/default-settings/files/zzz-default-settings
+sed -i '/firewall.user/d' package/lean/default-settings/files/zzz-default-settings
